e5ef27aee1cc3ead7d508378d6d33c860a0c222a
MarkBorca <60783220+MarkBorca@users.noreply.github.com>
2022-02-23 16:10:44 -0800

    -

--- db.py
+++ db.py
@@ -1,9 +1,6 @@
 from pymongo import MongoClient
 import pymongo
-from datetime import datetime
 import datetime
-import time
-
 
 class MongoConnector:
     """
@@ -19,8 +16,12 @@ class MongoConnector:
         self.envirenment = self._db.envirenment
         self.setings = self._db.setings
 
-        self.today = datetime.today().strftime('%Y-%m-%d')
-        self.time = datetime.today().strftime('%H:%M:%S')
+        self.get_time()
+
+    def get_time(self):
+        self.date = datetime.datetime.today().strftime('%Y-%m-%d')
+        self.time = datetime.datetime.today().strftime('%H:%M:%S')
+        self.date_time = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
     def current_position(self):
         """
@@ -28,19 +29,20 @@ class MongoConnector:
         :param date: current date
         :return: last position to tracker, date and time
         """
-        same_date = list(self._db.gps.find({'date': self.today}))
+        same_date = list(self._db.gps.find({'date': self.date}))
         return same_date[len(same_date) - 1]
 
-    def immobile(self, time=7200):
+    def immobile(self, time='2022-02-23 12:48:44'):
         """
         if tracker has not moved for x time notify
-        :param time: int - 2h by default
+        :param time: int - 2h by default, writen in minutes
         :return: position to tracker
         """
         # if self.current_position()['date'] == self.today:
         # if self.time - self.current_position()['time']:
         # pass
-        return datetime.timedelta('01.00.59').total_seconds()
+
+        return self.date_time < time
 
 
 if __name__ == '__main__':
